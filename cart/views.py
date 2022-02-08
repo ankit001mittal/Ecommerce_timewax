50c747a1182e85be5267d7db05836d663315f68a
ankit001mittal <47357449+ankit001mittal@users.noreply.github.com>
2022-02-08 12:09:40 +0530

    Add files via upload

---
+++ cart/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
+from coupons.forms import CouponApplyForm
+
+
+@require_POST
+@csrf_exempt
+def cart_add(request, id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
+    return redirect('cart:cart_detail')
+
+@csrf_exempt
+def cart_remove(request, id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+@csrf_exempt
+def cart_detail(request):
+    cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'override': True})
+    coupon_apply_form = CouponApplyForm()
+    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
+
